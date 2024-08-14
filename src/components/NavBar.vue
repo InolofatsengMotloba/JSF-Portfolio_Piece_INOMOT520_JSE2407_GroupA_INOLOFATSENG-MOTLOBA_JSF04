f76070ef91d54513abb3dda7c93cce44bc9e1505
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-14 11:22:10 +0200

    Added a Compare List menu item to the navigation bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -60,6 +60,14 @@
           >
             <li>
               <a
+                href="/compare"
+                class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+              >
+                Compare Items
+              </a>
+            </li>
+            <li>
+              <a
                 href="/wishlist"
                 class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >

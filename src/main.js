e60c75127bc06028b505e29e86a6508a7244b9a2
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-15 13:07:47 +0200

    Integrate Vuex store into the main application

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import store from "./store/loginStore";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+createApp(App).use(createPinia()).use(router).use(store).mount("#app");

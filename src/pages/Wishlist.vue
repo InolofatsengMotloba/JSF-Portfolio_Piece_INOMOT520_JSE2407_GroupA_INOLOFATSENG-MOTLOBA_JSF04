f2dd08f1b0b6aa594da523c723cae99998744744
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-15 14:29:42 +0200

    Added computed property to check user login status using Vuex store & added conditional header and login message to Wishlist.vue

--- src/pages/Wishlist.vue
+++ src/pages/Wishlist.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <h1>WishList Page</h1>
+    <h1 v-if="isLoggedIn">WishList Page</h1>
+    <p v-else>Please log in to view items in Wishlist</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "WishList",
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
+    },
+  },
 };
 </script>

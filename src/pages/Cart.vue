9cd20b6f8760f89b493d5930ce23c346d7ed656b
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-15 14:26:50 +0200

    Added computed property to check user login status using Vuex store & added conditional header and login message to Cart.vue

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <h1>Add to Cart Page</h1>
+    <h1 v-if="isLoggedIn">Add to Cart Page</h1>
+    <p v-else>Please log in to view items in cart</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Cart",
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
+    },
+  },
 };
 </script>

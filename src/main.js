ba97c762b557714a04bd14daf909f9424cdfb7a2
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-19 13:26:23 +0200

    Initialize cart store, load cart data, and set user ID using auth token in main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import store from "./store/loginStore";
+import { useCartStore } from "./store/cartStore";
 
 createApp(App).use(createPinia()).use(router).use(store).mount("#app");
+
+// Initialize cart store and set user ID
+const cartStore = useCartStore();
+cartStore.loadCart();
+cartStore.setUserId(store.state.authToken);

9440c2e0923bed94910f2064e07b8cc9e1ea8e90
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-20 00:33:42 +0200

    Updated compare list page UI: Add Please Login message with login button

--- src/pages/CompareList.vue
+++ src/pages/CompareList.vue
@@ -26,12 +26,18 @@
                     :src="product.image"
                     :alt="product.title"
                   />
-                  <span class="font-semibold text-slate-600">{{ product.title }}</span>
+                  <span class="font-semibold text-slate-600">{{
+                    product.title
+                  }}</span>
                 </div>
               </td>
-              <td class="p-4 border-b text-gray-700 mb-3">{{ product.description }}</td>
+              <td class="p-4 border-b text-gray-700 mb-3">
+                {{ product.description }}
+              </td>
               <td class="p-4 border-b text-slate-500">${{ product.price }}</td>
-              <td class="p-4 border-b text-gray-700 mb-3">⭐ {{ product.rating?.rate }}</td>
+              <td class="p-4 border-b text-gray-700 mb-3">
+                ⭐ {{ product.rating?.rate }}
+              </td>
               <td class="p-4 border-b">
                 <button
                   @click="removeFromCompareList(product.id)"
@@ -53,9 +59,26 @@
         </button>
       </div>
     </div>
-    <div v-else>
-      <h1>Please log in to view items in Comparison List</h1>
-      <button><a href="/login">Login</a></button>
+    <div v-else class="grid m-10 space-y-5">
+      <div
+        class="flex flex-col items-center bg-white border-2 border-gray-500 p-4"
+      >
+        <img
+          class="object-contain h-48 mt-3 mb-3"
+          src="/public/images/NotLogIn.png"
+          alt="Empty Cart"
+        />
+        <p
+          class="text-lg line-clamp-2 font-extrabold leading-snug text-slate-600"
+        >
+          Please log in to view items in Comparison List
+        </p>
+        <button
+          class="flex rounded justify-center mt-3 bg-[#381257] px-3 py-2 text-sm font-medium text-white hover:bg-violet-500 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300"
+        >
+          <a href="/login">Login</a>
+        </button>
+      </div>
     </div>
   </div>
 </template>

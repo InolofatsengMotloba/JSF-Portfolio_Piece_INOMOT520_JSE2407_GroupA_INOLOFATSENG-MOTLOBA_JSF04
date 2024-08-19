04faa49fa5a0a2b47e9f855dd7bcd50fbd0e8421
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-19 23:35:15 +0200

    Add function to save compare list to local storage to CompareStore.js

--- src/store/compareStore.js
+++ src/store/compareStore.js
@@ -3,5 +3,13 @@ import { ref } from "vue";
 
 export const useCompareStore = defineStore("compare", () => {
   const compareList = ref([]);
-  
+
+  // Save compare list to local storage
+  const saveCompareList = () => {
+    localStorage.setItem("compareList", JSON.stringify(compareList.value));
+  };
+
+  return {
+    compareList,
+  };
 });

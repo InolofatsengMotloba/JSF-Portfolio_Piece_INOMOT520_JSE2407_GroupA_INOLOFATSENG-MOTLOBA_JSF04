e7cbeb5afc7139074f9a88473e61aafa4fbc62a0
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-19 11:40:39 +0200

    Add function to set user ID from JWT token & return store method and properties

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -6,4 +6,16 @@ export const useCartStore = defineStore("cart", () => {
   const cart = ref([]);
   const userId = ref(null);
 
+  const setUserId = (token) => {
+    if (token) {
+      const decoded = jwtDecode(token);
+      userId.value = decoded.userId;
+    }
+  };
+
+  return {
+    cart,
+    userId,
+    setUserId,
+  };
 });

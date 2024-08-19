29343915cec3453d800a493abb409520f8b0b5ea
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-19 23:38:10 +0200

    Add function to load compare list from local storage & Implement add to compare list functionality

--- src/store/compareStore.js
+++ src/store/compareStore.js
@@ -4,12 +4,30 @@ import { ref } from "vue";
 export const useCompareStore = defineStore("compare", () => {
   const compareList = ref([]);
 
+  // Load compare list from local storage
+  const loadCompareList = () => {
+    const storedList = JSON.parse(localStorage.getItem("compareList"));
+    if (storedList) {
+      compareList.value = storedList;
+    }
+  };
+
   // Save compare list to local storage
   const saveCompareList = () => {
     localStorage.setItem("compareList", JSON.stringify(compareList.value));
   };
 
+  // Add a product to the compare list
+  const addToCompareList = (product) => {
+    if (compareList.value.length >= 4) {
+      alert("You can only compare up to 4 items.");
+      return;
+    }
+  };
+
   return {
     compareList,
+    addToCompareList,
+    loadCompareList,
   };
 });

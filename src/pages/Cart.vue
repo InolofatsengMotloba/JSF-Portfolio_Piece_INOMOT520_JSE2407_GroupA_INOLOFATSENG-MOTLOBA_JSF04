c2d8818c81e2e047f7f7320bfa61a98896a00e52
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-19 14:09:36 +0200

    Updated Cart.vue to display cart items, total price, and added functionality to update quantity, remove items, and clear cart

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
     <div v-if="isLoggedIn">
-      <h1>Add to Cart Page</h1>
+      <div v-if="cartItems.length">
+        <div
+          v-for="item in cartItems"
+          :key="item.product.id"
+          class="flex justify-between items-center my-4"
+        >
+          <div>
+            <h3>{{ item.product.title }}</h3>
+            <p>{{ item.product.price }} x {{ item.quantity }}</p>
+            <input
+              type="number"
+              :value="item.quantity"
+              @input="updateQuantity(item.product.id, $event.target.value)"
+            />
+            <button @click="removeItem(item.product.id)">Remove</button>
+          </div>
+        </div>
+        <div>
+          <p>Total: {{ totalPrice }}</p>
+          <button @click="clearCart">Clear Cart</button>
+        </div>
+      </div>
+      <div v-else>
+        <p>Your cart is empty</p>
+      </div>
     </div>
     <div v-else>
       <h1>Please log in to view items in cart</h1>
@@ -11,8 +35,26 @@
 </template>
 
 <script>
+import { useCartStore } from "../store/cartStore";
+import { computed } from 'vue';
+import { storeToRefs } from 'pinia';
+
 export default {
   name: "Cart",
+  setup() {
+    const cartStore = useCartStore();
+    const { cart, totalPrice } = storeToRefs(cartStore);
+
+    const cartItems = computed(() => cart.value);
+
+    return {
+      cartItems,
+      totalPrice,
+      updateQuantity: cartStore.updateQuantity,
+      removeItem: cartStore.removeItem,
+      clearCart: cartStore.clearCart,
+    };
+  },
   computed: {
     isLoggedIn() {
       return this.$store.getters.isLoggedIn;

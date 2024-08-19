645579176a1eb7f993314841fa73aad80f49869b
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-19 08:56:45 +0200

    Added computed property to check user login status using Vuex store & added conditional header and login message to CompareList.vue

--- src/pages/CompareList.vue
+++ src/pages/CompareList.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <h1>Compare List Page</h1>
+    <div v-if="isLoggedIn">
+      <h1>Compare List Page</h1>
+    </div>
+    <div v-else>
+      <h1>Please log in to view Compare List</h1>
+      <a href="/login"><button>Login</button></a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "CompareList",
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
+    },
+  },
 };
 </script>

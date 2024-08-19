244376a2b3fd1752c4c03733716f6ee0e0f6d1a8
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-19 09:32:12 +0200

    Create a cartStore using Pinia to manage the cart functionality

---
+++ src/store/cartStore.js
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import { jwtDecode } from "jwt-decode";
+
+export const useCartStore = defineStore('cart', () => {
+    
+} )

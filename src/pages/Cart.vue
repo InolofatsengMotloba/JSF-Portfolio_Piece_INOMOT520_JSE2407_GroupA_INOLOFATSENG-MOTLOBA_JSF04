9b921b2bf3fd6fe8406a89ba7f72e2417a15a38c
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-19 15:21:14 +0200

    Updated cart page UI: Add empty cart message with continue shopping button to Cart.vue & formatt code

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -23,8 +23,27 @@
           <button @click="clearCart">Clear Cart</button>
         </div>
       </div>
-      <div v-else>
-        <p>Your cart is empty</p>
+
+      <div v-else class="grid m-10 space-y-5">
+        <div
+          class="flex flex-col items-center bg-white border-2 border-gray-500 p-4"
+        >
+          <img
+            class="object-contain h-48 mt-3 mb-3"
+            src="/public/images/EmptyCart.png"
+            alt="Empty Cart"
+          />
+          <p
+            class="text-lg line-clamp-2 font-extrabold leading-snug text-slate-600"
+          >
+            Your shopping cart is empty
+          </p>
+          <button
+            class="flex rounded justify-center mt-3 bg-[#381257] px-3 py-2 text-sm font-medium text-white hover:bg-violet-500 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300"
+          >
+            <a href="/">Continue Shopping</a>
+          </button>
+        </div>
       </div>
     </div>
     <div v-else>
@@ -36,8 +55,8 @@
 
 <script>
 import { useCartStore } from "../store/cartStore";
-import { computed } from 'vue';
-import { storeToRefs } from 'pinia';
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
 
 export default {
   name: "Cart",

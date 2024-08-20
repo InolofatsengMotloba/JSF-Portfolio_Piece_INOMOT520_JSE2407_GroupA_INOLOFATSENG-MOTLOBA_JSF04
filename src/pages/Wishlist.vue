95a4adf14dc836738fd70c6bdf70053edbc2d63c
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-20 16:13:16 +0200

    Refactor Wishlist.vue: updated layout, styling, and button designs for wishlist items and clear all button

--- src/pages/Wishlist.vue
+++ src/pages/Wishlist.vue
@@ -22,14 +22,99 @@
           </button>
         </div>
       </div>
-      <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <button @click="clearWishlist">Clear All</button>
-        <div v-for="item in wishlist" :key="item.id" class="p-4 border">
-          <img :src="item.image" alt="item.title" />
-          <h2>{{ item.title }}</h2>
-          <p>{{ item.price }}</p>
-          <button @click="removeFromWishlist(item.id)">Remove</button>
-          <button @click="addToCart(item)">Add to Cart</button>
+      <div v-else>
+        <button
+          @click="clearWishlist"
+          class="bg-red-500 my-5 w-auto ml-10 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:bg-red-400 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-300"
+        >
+          Clear All
+        </button>
+        <div class="grid justify-center">
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+            <div
+              v-for="item in wishlist"
+              :key="item.id"
+              class="flex flex-col max-h-[130rem] cursor-pointer max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
+            >
+              <img
+                :src="item.image"
+                :alt="item.title"
+                class="object-contain h-48 mt-3"
+              />
+              <div class="flex-1 flex flex-col p-6">
+                <div class="flex-1">
+                  <header class="mb-2 flex-2">
+                    <h2
+                      class="text-lg line-clamp-2 font-extrabold leading-snug text-slate-600"
+                    >
+                      {{ item.title }}
+                    </h2>
+                  </header>
+                </div>
+
+                <!-- Ratings -->
+                <div class="flex items-left -ml-2 mb-2">
+                  <template
+                    v-for="index in Math.round(item.rating.rate)"
+                    :key="index"
+                  >
+                    <svg
+                      class="w-4 h-4 text-yellow-300 ms-1"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 22 20"
+                    >
+                      <path
+                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+                      />
+                    </svg>
+                  </template>
+                  <template
+                    v-for="index in 5 - Math.round(item.rating.rate)"
+                    :key="index"
+                  >
+                    <svg
+                      class="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 22 20"
+                    >
+                      <path
+                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+                      />
+                    </svg>
+                  </template>
+                </div>
+
+                <div
+                  class="text-base line-clamp-2 font-extrabold text-slate-500 leading-snug"
+                >
+                  <h2>$ {{ item.price }}</h2>
+                </div>
+                <div>
+                  <span
+                    class="inline-flex items-center rounded-md bg-blue-100 px-2 py-1 text-xs font-medium text-black ring-1 ring-inset ring-blue-700/10 mt-2"
+                    >{{ item.category }}</span
+                  >
+                </div>
+
+                <button
+                  @click="removeFromWishlist(item.id)"
+                  class="flex rounded-lg justify-center mt-3 bg-red-500 px-3 py-2 text-sm font-medium text-white hover:bg-red-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300"
+                >
+                  Remove Item
+                </button>
+                <button
+                  @click="addToCart(item)"
+                  class="flex rounded-lg justify-center mt-3 bg-[#381257] px-3 py-2 text-sm font-medium text-white hover:bg-violet-500 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

f4f936049711cba3a04079c1a7ff7b5bfb995a64
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-20 15:35:37 +0200

    Updated Wishlist page UI with new empty wishlist message and styling

--- src/pages/Wishlist.vue
+++ src/pages/Wishlist.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
     <div v-if="isLoggedIn">
-      <h1>WishList Page</h1>
-      <button @click="clearWishlist">Clear All</button>
-      <div v-if="wishlist.length === 0">Your wishlist is empty.</div>
+      <div v-if="wishlist.length === 0" class="grid m-10 space-y-5">
+        <div
+          class="flex flex-col items-center bg-white border-2 border-gray-500 p-4"
+        >
+          <img
+            class="object-contain h-48 mt-3 mb-3"
+            src="/public/images/WishList.png"
+            alt="Empty Cart"
+          />
+          <p
+            class="text-lg line-clamp-2 font-extrabold leading-snug text-slate-600"
+          >
+            Your Wishlist is empty
+          </p>
+          <button
+            class="flex rounded justify-center mt-3 bg-[#381257] px-3 py-2 text-sm font-medium text-white hover:bg-violet-500 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300"
+          >
+            <a href="/">Continue Shopping</a>
+          </button>
+        </div>
+      </div>
       <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <button @click="clearWishlist">Clear All</button>
         <div v-for="item in wishlist" :key="item.id" class="p-4 border">
           <img :src="item.image" alt="item.title" />
           <h2>{{ item.title }}</h2>
@@ -13,7 +32,6 @@
           <button @click="addToCart(item)">Add to Cart</button>
         </div>
       </div>
-      
     </div>
     <div v-else class="grid m-10 space-y-5">
       <div

70acd7309744458455db6d208d6ca3cf27dfdeec
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-20 15:30:44 +0200

    Added wishlist page functionality, including display of wishlist items, clear all button, and add to cart/remove from wishlist buttons

--- src/pages/Wishlist.vue
+++ src/pages/Wishlist.vue
@@ -2,6 +2,18 @@
   <div>
     <div v-if="isLoggedIn">
       <h1>WishList Page</h1>
+      <button @click="clearWishlist">Clear All</button>
+      <div v-if="wishlist.length === 0">Your wishlist is empty.</div>
+      <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div v-for="item in wishlist" :key="item.id" class="p-4 border">
+          <img :src="item.image" alt="item.title" />
+          <h2>{{ item.title }}</h2>
+          <p>{{ item.price }}</p>
+          <button @click="removeFromWishlist(item.id)">Remove</button>
+          <button @click="addToCart(item)">Add to Cart</button>
+        </div>
+      </div>
+      
     </div>
     <div v-else class="grid m-10 space-y-5">
       <div
@@ -30,12 +42,33 @@
 </template>
 
 <script>
+import { useWishlistStore } from "../store/wishlistStore";
+import { useCartStore } from "../store/cartStore";
+import { storeToRefs } from "pinia";
+
 export default {
   name: "WishList",
   computed: {
     isLoggedIn() {
       return this.$store.getters.isLoggedIn;
     },
+  },
+  setup() {
+    const wishlistStore = useWishlistStore();
+    const cartStore = useCartStore();
+    const { wishlist } = storeToRefs(wishlistStore);
+
+    function addToCart(item) {
+      cartStore.addToCart(item);
+      // wishlistStore.removeFromWishlist(item.id);
+    }
+
+    return {
+      wishlist,
+      removeFromWishlist: wishlistStore.removeFromWishlist,
+      clearWishlist: wishlistStore.clearWishlist,
+      addToCart,
+    };
   },
 };
 </script>

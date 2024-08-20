3de567f709d1747fefeca51d6c66a7f6f114fa1e
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-20 15:07:53 +0200

    Initialize wishlist state from localStorage or empty array

--- src/store/wishlistStore.js
+++ src/store/wishlistStore.js
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia";
 
 export const useWishlistStore = defineStore("wishlist", {
-  
+  state: () => ({
+    wishlist: JSON.parse(localStorage.getItem("wishlist")) || [],
+  }),
 });

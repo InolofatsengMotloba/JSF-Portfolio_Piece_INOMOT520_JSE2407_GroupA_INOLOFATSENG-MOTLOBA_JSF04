67d6fa1d05b83e4d47a6f77dd772a35ed64e1fc5
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-20 15:08:38 +0200

    Add getters for wishlist count and checking if product is in wishlist

--- src/store/wishlistStore.js
+++ src/store/wishlistStore.js
@@ -4,4 +4,10 @@ export const useWishlistStore = defineStore("wishlist", {
   state: () => ({
     wishlist: JSON.parse(localStorage.getItem("wishlist")) || [],
   }),
+  getters: {
+    wishlistCount: (state) => state.wishlist.length,
+    isInWishlist: (state) => (product) => {
+      return state.wishlist.some((item) => item.id === product.id);
+    },
+  },
 });
